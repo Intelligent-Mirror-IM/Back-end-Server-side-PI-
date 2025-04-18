f6e23e5107125799891e7e6ade4d4aeb64928eba
mAYA <32102333097@std.bau.edu.jo>
2025-04-18 13:42:10 +0100

    Nice

--- index.js
+++ index.js
@@ -2,24 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-// Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/chatbotDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Define a schema
-const messageSchema = new mongoose.Schema({
-  message: String,
-});
-
-// Create a model
-const Message = mongoose.model("Message", messageSchema);
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+app.get("/",(req,res) =>
+{
+	res.status(200).json({message:"It's working"});
+});
 app.get("/api/chatbot", (req, res) => {
   res.json({ message: "Hello, World" });
 });
