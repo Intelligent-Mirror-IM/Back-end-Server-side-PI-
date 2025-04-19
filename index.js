ada112e1e358eaf76e28bd983cbbd738425f5f66
Proton <basselawni1@gmail.com>
2025-04-19 10:30:40 +0300

    Database Connections

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+mongoose.createConnection(process.env.MONGO_URL).asPromise().then(() => {
+  console.log("MongoDB connected");
+}
+).catch((err) => {
+  console.log("MongoDB connection error:", err);
+}
+);
+mongoose.connection.on("error", (err) => {
+  console.log("MongoDB connection error:", err);
+}
+);
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB disconnected");
+}
+);
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen( PORT , () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.get("/",(req,res) =>
